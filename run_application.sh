0ac891a85fd6001deca2d6529468ca5685cf1716
Orel Levi <66939928+Orelevi@users.noreply.github.com>
2023-10-03 13:51:33 +0300

    Update run_application.sh

--- run_application.sh
+++ run_application.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Step 1: Pull the latest changes from the repository
-git pull
+git clone https://github.com/orelevi/PayPal_Home_assignment.git
 
 # Step 2: Change to the project directory
-cd /path/to/your/project/directory
+cd PayPal_Home_assignment
 
 # Step 3: Run docker-compose to build and start the containers
 docker-compose up --build
+
